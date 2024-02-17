f9d3317f1a493e5ffdf75987625df7c79458b03f
Ilailson <ilailson@hotmail.com>
2024-02-17 10:42:46 -0300

    Aprendendo databingins

---
+++ databingins/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  students: { type: string; name: string; description: string; }[] = [];
+  newStudentName = '';
+  newStudentDescription = '';
+
+  addApproved() {
+    this.students.push({
+      type: 'approved',
+      name: this.newStudentName,
+      description: this.newStudentDescription
+    })
+  }
+
+  addSubstitute() {
+    this.students.push({
+      type: 'substitute',
+      name: this.newStudentName,
+      description: this.newStudentDescription
+    })
+  }
+}
