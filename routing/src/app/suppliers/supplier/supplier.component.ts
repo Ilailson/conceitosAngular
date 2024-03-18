57f13a1560c2df15fca68fb06c812387118f12e7
ilailsonrocha <ilailsonrocha@iec.gov.br>
2024-03-18 17:13:35 -0300

    Utilizando Observable

--- routing/src/app/suppliers/supplier/supplier.component.ts
+++ routing/src/app/suppliers/supplier/supplier.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-supplier',
@@ -7,17 +7,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./supplier.component.css']
 })
 export class SupplierComponent implements OnInit {
+
   supplier?: {
     id:number,
     name: string    
   }
+
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-      this.supplier = {
-        id: this.route.snapshot.params['id'],
-        name: this.route.snapshot.params['name']
-      };
+      this.route.params.subscribe(
+        (params: Params)=> {
+          this.supplier = {
+            id: params['id'],
+            name: params['name']
+          }
+        }
+      )
     }
   }
 
